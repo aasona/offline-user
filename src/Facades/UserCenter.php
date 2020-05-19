395c22e23a9e914ca5b3ccd19aada819a599468c
yuanhb <3345718878@qq.com>
2020-05-19 10:11:38 +0800

    外观模式追加

---
+++ src/Facades/UserCenter.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * User: yuanHb <1214846385@qq.com>
+ * Date: 2020/5/19
+ * Time: 10:09.
+ */
+
+
+namespace Xthk\Ucenter\offline\Facades;
+
+
+class UserCenter extends Illuminate\Support\Facades\Facade
+{
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'offlineUser';
+    }
+}
