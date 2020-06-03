55c5ae780e7562200869c8989a31067c9a3aa740
yuanhb <3345718878@qq.com>
2020-06-03 18:18:17 +0800

    日志追加

--- src/UserCenterConnect.php
+++ src/UserCenterConnect.php
@@ -181,6 +181,7 @@ class UserCenterConnect extends OperateUcenter
     public function response($result, $type = 'array')
     {
         \Log::info('请求地址>>>' . json_encode($this->config['uri']));
+        \Log::info('配置参数>>>' . json_encode($this->config));
         \Log::info(json_encode($this->params));
         \Log::info($result);
         $result = json_decode($result, true);
